5cef08d26d5e9a265e2036951015b0b13740fa8f
Lannn3x <alancasario09@gmail.com>
2025-10-25 20:22:58 -0300

    Create EX 009.py
    EX 009

---
+++ EX 009.py
@@ -0,0 +1,20 @@
+#Exercício Python 009: Faça um programa que leia um número Inteiro qualquer e mostre na tela a sua tabuada.
+
+n1 = int(input("Digite um numero para ver a tabuada: "))
+
+print('-' * 20)
+
+print("A Tabuada Do {} É ".format(n1))
+
+print("{} X 1 = {}".format(n1,n1*1))
+print("{} X 2 = {}".format(n1,n1*2))
+print("{} X 3 = {}".format(n1,n1*3))
+print("{} X 4 = {}".format(n1,n1*4))
+print("{} X 5 = {}".format(n1,n1*5))
+print("{} X 6 = {}".format(n1,n1*6))
+print("{} X 7 = {}".format(n1,n1*7))
+print("{} X 8 = {}".format(n1,n1*8))
+print("{} X 9 = {}".format(n1,n1*9)) 
+print("{} X 10 = {}".format(n1,n1*10))
+
+print('-' * 20)
