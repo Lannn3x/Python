3c65ebfe8ff3530e425dbc4eeffbcd532645d00e
Lannn3x <alancasario09@gmail.com>
2025-10-25 20:13:23 -0300

    Create EX 002.py
    EX 002

---
+++ EX 002.py
@@ -0,0 +1,5 @@
+#Exercício Python 002: Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas.
+
+name = input("Qual é o seu nome? ")
+
+print ("Ola {}! Seja Bem Vindo(a)!".format(name))
