834fc58ce1964273a1e4017458f20435ea31ff69
Lannn3x <alancasario09@gmail.com>
2025-10-25 20:13:42 -0300

    Create EX 004.py
    EX 004

---
+++ EX 004.py
@@ -0,0 +1,25 @@
+#Exercício Python 004: Faça um programa que leia algo pelo teclado e mostre na tela o seu tipo primitivo e todas as informações possíveis sobre ele.
+
+algo = input("Digite algo: ")
+print("O tipo primitivo desse valor é:", type(algo))
+
+espaco = algo.isspace()
+print("Só tem espaços?", espaco)
+
+numero = algo.isnumeric()
+print("É um número?", numero)
+
+alfanumerico = algo.isalnum()
+print("É alfanumerico?", alfanumerico)
+
+alfabetico = algo.isalpha()
+print("É alfabetico?", alfabetico)
+
+maiúsculas = algo.isupper()
+print("Está em maiúsculas?", algo.isupper())
+
+minusculas = algo.islower()
+print("Está em minusculas?", algo.islower())
+
+capitalizada = algo.istitle()
+print("Está capitalizada?", algo.istitle())
