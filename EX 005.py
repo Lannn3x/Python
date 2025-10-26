e60930b5b2963ce595a339d8829a348b76995924
Lannn3x <alancasario09@gmail.com>
2025-10-25 21:23:04 -0300

    Update EX 005.py
    EX 005

--- EX 005.py
+++ EX 005.py
@@ -1,6 +1,9 @@
 #Exercício Python 005: Faça um programa que leia um número Inteiro 
 # e mostre na tela o seu sucessor e seu antecessor.
 
+#Python Exercise 005: Write a program that reads an integer
+# and displays its successor and predecessor on the screen.
+
 num = int(input("Digite um número: "))
 
 print("analisando o valor {}, seu antecessor é {} e o sucessor é {}".format(num, (num-1), (num+1)))
