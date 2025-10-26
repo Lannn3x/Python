80e4dfa297c7f5bd870d1509a9b00e812b909bd9
Lannn3x <alancasario09@gmail.com>
2025-10-25 21:23:19 -0300

    Update EX 007.py
    EX 007

--- EX 007.py
+++ EX 007.py
@@ -1,5 +1,7 @@
 #Exercício Python 007: Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média.
 
+#Python Exercise 007: Develop a program that reads a student's two grades, calculates and displays their average.
+
 num1 = float(input("Digite a primeira nota: "))
 
 num2 = float(input("Digite a segunda nota: "))
