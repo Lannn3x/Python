def8bd935b57ad88fc9c5b41bbe52721fdeee876
Lannn3x <alancasario09@gmail.com>
2025-10-25 21:24:05 -0300

    Update EX 012.py
    EX 012

--- EX 012.py
+++ EX 012.py
@@ -1,5 +1,7 @@
 #Exercício Python 012: Faça um algoritmo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto.
 
+#Python Exercise 012: Create an algorithm that reads the price of a product and shows its new price, with a 5% discount.
+
 n1 = float(input("Digite O Valor Do Produto? $:"))
 
 c1 = n1 - (n1 * 5 / 100)
