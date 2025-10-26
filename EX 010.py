b918f387acb74da121da080e10dfe95da071e457
Lannn3x <alancasario09@gmail.com>
2025-10-25 21:23:48 -0300

    Update EX 010.py
    EX 010

--- EX 010.py
+++ EX 010.py
@@ -1,5 +1,7 @@
 #Exercício Python 010: Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dólares ela pode comprar.
 
+#Python Exercise 010: Create a program that reads how much money a person has in their wallet and shows how many dollars they can buy.
+
 n1 = float(input("Digite Um Valor De Dinheiro Para Converter Para US$: "))
 
 n2 = (n1 / 5.39)
