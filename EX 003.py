ca9d4aa7147c9c76e68c21a4f0c50cca35cac44a
Lannn3x <alancasario09@gmail.com>
2025-10-25 21:22:51 -0300

    Update EX 003.py
    EX 003

--- EX 003.py
+++ EX 003.py
@@ -1,5 +1,7 @@
 #Exercício Python 003: Crie um programa que leia dois números e mostre a soma entre eles.
 
+#Python Exercise 003: Create a program that reads two numbers and displays the sum between them.
+
 num1 = int(input("Digite o primeiro número: "))
 
 num2 = int(input("Digite o segundo número: "))
