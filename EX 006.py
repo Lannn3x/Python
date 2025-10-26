3e00a93a4ca0fa8a86e434c5b741a16c3f54f1e9
Lannn3x <alancasario09@gmail.com>
2025-10-25 21:23:12 -0300

    Update EX 006.py
    EX 006

--- EX 006.py
+++ EX 006.py
@@ -1,5 +1,7 @@
 #Exercício Python 006: Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada.
 
+#Python Exercise 006: Create an algorithm that reads a number and displays its double, triple, and square root.
+
 num = int(input("Digite um número: "))
 
 print("o dobro de {} é {}".format(num, (num*2)))
