a0095348952c583166bc94251641cf1531274871
Lannn3x <alancasario09@gmail.com>
2025-10-25 21:24:20 -0300

    Update EX 014.py
    EX 014

--- EX 014.py
+++ EX 014.py
@@ -1,5 +1,7 @@
 #Exercício Python 014: Escreva um programa que converta uma temperatura digitando em graus Celsius e converta para graus Fahrenheit.
 
+#Python Exercise 014: Write a program that converts a temperature by typing in degrees Celsius and converts it to degrees Fahrenheit.
+
 c = float(input("Informe A Temperatura Em °C: "))
 
 f= (1.8 * c) + 32
