fdeac044160b78315f7e1e7ced23491febecac7a
Lannn3x <alancasario09@gmail.com>
2025-10-25 21:23:31 -0300

    Update EX 008.py
    EX 008

--- EX 008.py
+++ EX 008.py
@@ -1,5 +1,7 @@
 #Exercício Python 008: Escreva um programa que leia um valor em metros e o exiba convertido em centímetros e milímetros.
 
+#Python Exercise 008: Write a program that reads a value in meters and displays it converted to centimeters and millimeters.
+
 n1 = float(input('digite um valor em metros:'))
 
 n2=n1*100
