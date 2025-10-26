24918ddd4ea8bd8136504a3410bb41988f4b9337
Lannn3x <alancasario09@gmail.com>
2025-10-26 19:24:12 -0300

    Create EX 021.py
    EX 021

---
+++ EX 021.py
@@ -0,0 +1,10 @@
+#Exercício Python 021: Faça um programa em Python que abra e reproduza o áudio de um arquivo MP3.
+
+#Python Exercise 021: Write a Python program that opens and plays the audio from an MP3 file.
+
+import playsound
+
+playsound.playsound('C:/Users/alanc/OneDrive/Documentos/Python/Python/EX 021.mp3')
+
+#Legenda PTBR:Não ha outro DEUS senão JESUS CRISTO, Não ha outro Juiz senão JESUS CRISTO, Não ha outra Maneira senão JESUS CRISTO, Você não pode escapar DELE,
+#Não pode se ESCONDER DELE, Não pode MENTIR para ELE,Não pode SUBORNA-LO,Não pode ENGANÁ-LO,Você não pode colocar nenhum PLANO contra ELE e achar que vão PROSPERAR
