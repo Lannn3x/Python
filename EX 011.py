567602aaf46efc5844a438fadc8f72ec7b9e91b3
Lannn3x <alancasario09@gmail.com>
2025-10-25 21:23:55 -0300

    Update EX 011.py
    EX 011

--- EX 011.py
+++ EX 011.py
@@ -1,6 +1,9 @@
 #Exercício Python 011: Faça um programa que leia a largura e a altura de uma parede em metros,calcule a sua área e a quantidade de tinta necessária para pintá-la,
 #sabendo que cada litro de tinta pinta uma área de 2 metros quadrados.
 
+#Python Exercise 011: Write a program that reads the width and height of a wall in meters, calculates its area, and the amount of paint needed to paint it,
+#knowing that each liter of paint paints an area of ​​2 square meters.
+
 n1 = float(input("Largura Da Parede: "))
 
 n2 = float(input("Altura Da Parede: "))
