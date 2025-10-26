e481f9375f26ae116ca9c441f508a6510e5362ea
Lannn3x <alancasario09@gmail.com>
2025-10-25 21:24:12 -0300

    Update EX 013.py
    EX 013

--- EX 013.py
+++ EX 013.py
@@ -1,5 +1,7 @@
 #Exercício Python 013: Faça um algoritmo que leia o salário de um funcionário e mostre seu novo salário, com 15% de aumento.
 
+#Python Exercise 013: Create an algorithm that reads an employee's salary and shows their new salary, with a 15% increase.
+
 n1 = float(input("Qual O Salario Do Funcionario? $"))
 
 c1 = n1 + (n1 * 15 / 100)
