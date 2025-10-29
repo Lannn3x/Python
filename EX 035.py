7948b2214bc2b93f6de3227485d72e1228ac9e20
Lannn3x <alancasario09@gmail.com>
2025-10-29 20:31:27 -0300

    Update EX 035.py
    EX 035

--- EX 035.py
+++ EX 035.py
@@ -2,11 +2,15 @@
 
 #Python Exercise 035: Develop a program that reads the lengths of three lines and tells the user whether or not they can form a triangle.
 
-n1 = float(input("Primeiro Sergmento: "))
+print("\033[31m]-=-\033[m" * 20)
 
-n2 = float(input("Segundo Sergmento: "))
+n1 = float(input("Primeiro Sergmento: \033[m"))
 
-n3 = float(input("Terceiro Sergmento: "))
+n2 = float(input("Segundo Sergmento: \033[m"))
+
+n3 = float(input("Terceiro Sergmento: \033[m"))
+
+print("\033[31m]-=-\033[m" * 20)
 
 if n1 < n2 + n3 and n2 < n1 + n3 and n3 < n1 + n2:
 
